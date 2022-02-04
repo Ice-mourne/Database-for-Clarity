328cfbec0355e1cfeb5a93b718cbde79ae45bbcd
Icemourne <77716865+Ice-mourne@users.noreply.github.com>
2022-02-05 01:38:57 +0200

    Update descriptionInterface.ts

--- clarity_descriptions/descriptionInterface.ts
+++ clarity_descriptions/descriptionInterface.ts
@@ -32,7 +32,7 @@ export interface Description {
 
 export interface Table {
    lineText:   LineText[] // Row in table
-   className:  string // Css class name applies to table always 'table'
+   className:  string // Css class name applies to table // always 'table'
 }
 
 export interface LineText {
